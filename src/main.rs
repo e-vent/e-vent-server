f994e4a611e23bea9c57aeef3418dbca778ac27e
Daniel Tang <danielzgtg.opensource@gmail.com>
2018-11-11 01:58:23 -0500

    Add dummy events

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,23 @@ fn index() -> &'static str {
     "This is E-vent's backend server. You probably want to go to e-vent.github.io"
 }
 
+fn add_dummy_event(state: &EventBackend, name: &str, dec: &str, bg: &str) {
+    state.add(Event::from_details(
+        String::from(name),
+        String::from(desc),
+        bg,
+    ).unwrap());
+}
+
 fn main() {
+    state = EventBackend::new();
+
+    add_dummy_event(&state, "Breathing","The most popular event.", "desmarais");
+    add_dummy_event(&state, "Drinking","The second most popular event.", "desmarais");
+    add_dummy_event(&state, "Eating","The third most popular event.", "desmarais");
+
     rocket::ignite()
-        .manage(EventBackend::new())
+        .manage(state)
         .mount("/", routes![index])
         .launch();
 }
